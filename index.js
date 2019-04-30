f3738c6f43eaf9570730a7d4293bf77daac72d37
MUHM <m@ie.gl>
2019-04-30 23:24:46 +0800

    fix

--- index.js
+++ index.js
@@ -2,40 +2,33 @@
  * @Author: MUHM
  * @Date: 2019-04-30 16:10:23
  * @Last Modified by: MUHM
- * @Last Modified time: 2019-04-30 21:09:50
+ * @Last Modified time: 2019-04-30 23:24:36
  */
 'use strict';
 
 const BaseStorage = require('ghost-storage-base');
 const COS = require('cos-nodejs-sdk-v5');
 const path = require('path');
+const fs = require('fs');
 
 class TencentCOS extends BaseStorage {
   constructor(config) {
-    super()
-    const {
-      secretId,
-      secretKey,
-      bucket,
-      region,
-      baseUrl
-    } = config;
-    this.client = new COS({ secretId, secretKey });
-    this.bucket = bucket;
-    this.region = region;
-    this.baseUrl = baseUrl;
+    super(config)
+    this.client = new COS({ SecretId: config.secretId, SecretKey: config.secretKey });
+    this.config = config;
   }
 
   exists(filename, targetDir = this.getTargetDir('/')) {
-    const filepath = path.join(targetDir || this.getTargetDir(), filename);
-    return new Promise((resolve, reject) => {
-      this.client.headObject({
-        Bucket: this.bucket,
-        Region: this.region,
-        Key: path.resolve(targetDir, filename)
+    return new Promise((resolve) => {
+      // headOject方法 404
+      console.log(path.resolve(targetDir, filename));
+      this.client.getObjectAcl({
+        Bucket: this.config.bucket,
+        Region: this.config.region,
+        Key: path.resolve(targetDir, filename),
       }, (err) => {
-        if (err) {
-          reject(false);
+        if (err && err.error && err.error.Code === 'NoSuchKey') {
+          resolve(false);
         } else {
           resolve(true);
         }
@@ -44,19 +37,25 @@ class TencentCOS extends BaseStorage {
   }
 
   save(file, targetDir = this.getTargetDir('/')) {
-    const params = {
-      Bucket: this.bucket,
-      Region: this.region,
-      Key: this.getUniqueFileName(file, targetDir),
-      FilePath: file.path,
-    };
-    return new Promise((resolve, reject) => {
-      this.client.sliceUploadFile(params, (err, data) => {
-        if (err) {
-          reject(err);
-        }
-        resolve(this.baseUrl + data.Key);
+    const config = this.config;
+    const client = this.client;
+    return this.getUniqueFileName(file, targetDir).then(key => {
+      const params = {
+        Bucket: config.bucket,
+        Region: config.region,
+        Key: key,
+        Body: fs.createReadStream(file.path)
+      };
+      return new Promise((resolve, reject) => {
+        client.putObject(params, (err, data) => {
+          if (err) {
+            reject(err);
+          }
+          resolve(config.baseUrl + key);
+        });
       });
+    }).catch(e => {
+      return Promise.reject(e);
     });
   }
 
@@ -68,11 +67,11 @@ class TencentCOS extends BaseStorage {
 
   delete(filename, targetDir = this.getTargetDir('/')) {
     const params = {
-      Bucket: this.bucket,
-      Region: this.region,
+      Bucket: this.config.bucket,
+      Region: this.config.region,
       Key: path.resolve(targetDir, filename),
     };
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
       this.client.deleteObject(params, err => {
         if (err) {
           resolve(false);
